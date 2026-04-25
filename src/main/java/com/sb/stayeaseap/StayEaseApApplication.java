c52a75f5bde72a528f5ace589b88e062849d2eb9
Anas Mohamed <anasabdelrhman397@gmail.com>
2026-04-25 20:55:59 +0300

    initial commit

---
+++ src/main/java/com/sb/stayeaseap/StayEaseApApplication.java
@@ -0,0 +1,13 @@
+package com.sb.stayeaseap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayEaseApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayEaseApApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sb/stayeaseap/StayEaseApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.stayeaseap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayEaseApApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
